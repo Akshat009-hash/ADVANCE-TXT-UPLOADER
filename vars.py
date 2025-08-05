76237cee8f128cda17ea31365b636c8196f56a3b
Akshat009-hash <akshatofficial63@gmail.com>
2025-08-05 18:05:12 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,6 +3,6 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "24473318")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "7231089830:AAEtg4obq7JHvsZ0TcA-PSBi0vcJABV54e4") #Replace with your bot token
+API_ID = int(environ.get("API_ID", "26312639")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "312364f75fea3eec16ba69e0b655fc0a") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "7972048079:AAG1X0w1DSguU9aAR-TcTed7UM5To-Oye9E") #Replace with your bot token
